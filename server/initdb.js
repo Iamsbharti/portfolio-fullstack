8006fe92120b06b918a5aa088ca3717f58486fe8
saurabh <saurabhbharti9@gmail.com>
2020-09-16 10:09:32 +0530

    added initdb

---
+++ server/initdb.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const logger = require("./library/logger");
+
+exports.initdb = () => {
+  mongoose.connect(process.allowedNodeEnvironmentFlags.DB_CONNECT, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection.on("error", (error) => {
+    logger.error(`Error COnenctiong DB: ${error.message}`);
+  });
+  mongoose.connection.on("open", (error) => {
+    error
+      ? logger.error(`Error COnenctiong DB: ${error.message}`)
+      : logger.info("DB CONNECTION SUCCESS");
+  });
+};
