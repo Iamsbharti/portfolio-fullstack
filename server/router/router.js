ff6a22ee59347a92e99d7f2de1578effe5e30281
saurabh <saurabhbharti9@gmail.com>
2020-09-18 09:43:26 +0530

    added login route

--- server/router/router.js
+++ server/router/router.js
@@ -12,5 +12,6 @@ router.post("/portfolio/updateBlog", updateBlog);
 router.post("/portfolio/login", loginAdmin);
 */
 router.post("/portfolio/createUser", users.createUser);
+router.post("/portfolio/login", users.adminLogin);
 
 module.exports = router;
