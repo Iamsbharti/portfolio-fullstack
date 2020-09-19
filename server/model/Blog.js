561cf1928356361f9aea906f85199eb8753377b3
saurabh <saurabhbharti9@gmail.com>
2020-09-19 19:24:59 +0530

    refactored model

--- server/model/Blog.js
+++ server/model/Blog.js
@@ -5,6 +5,10 @@ const blogSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  userId: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
--- server/model/Project.js
+++ server/model/Project.js
@@ -5,6 +5,10 @@ const projectSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  userId: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
