425ec4dcbc1f4b59cfeb55e7d1e84a46ab3977cc
saurabh <saurabhbharti9@gmail.com>
2020-09-29 11:47:17 +0530

    added box styles to job_profile cards

--- client/src/components/profile/ProjectSection.js
+++ client/src/components/profile/ProjectSection.js
@@ -2,8 +2,30 @@ import React from "react";
 import "../../css/Project.css";
 const ProjectSection = () => {
   return (
-    <div>
-      <h1>ProjectSection Section...</h1>
+    <div className="project">
+      <div className="project__job__profile">
+        <div className="project__job__profile__card color__sec">
+          <i
+            className="fa fa-shield job__profile__icons"
+            aria-hidden="true"
+          ></i>
+          <h3>Security Consultant</h3>
+        </div>
+        <div className="project__job__profile__card">
+          <i
+            className="fa fa-desktop job__profile__icons"
+            aria-hidden="true"
+          ></i>
+          <h3>Full Stack Developer</h3>
+        </div>
+        <div className="project__job__profile__card color__ml">
+          <i
+            className="fa fa-line-chart job__profile__icons"
+            aria-hidden="true"
+          ></i>
+          <h3>Machine Learning Enthuiast</h3>
+        </div>
+      </div>
     </div>
   );
 };
