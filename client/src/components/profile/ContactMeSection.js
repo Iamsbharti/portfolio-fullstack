535f7f8e25d1044a0ae74343fe21e8a50ee00060
saurabh <saurabhbharti9@gmail.com>
2020-09-29 11:02:25 +0530

    refactored contact section component

--- client/src/components/profile/ContactMeSection.js
+++ client/src/components/profile/ContactMeSection.js
@@ -1,9 +1,141 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import "../../css/Profile.css";
+import NavBar from "./NavBar";
+import {
+  contactIntro,
+  contactInfoIntro,
+  emailIntro,
+  quote1Intro,
+  quote2Intro,
+  detailsIntro,
+  hoverLink,
+  hoverExit,
+} from "../Animation";
 
 const ContactMeSection = () => {
+  /**contact section */
+  let contact = useRef(null);
+  let contactInfo = useRef(null);
+  let email = useRef(null);
+
+  /**quoteSection */
+  let qoute1 = useRef(null);
+  let quote2 = useRef(null);
+  let exp = useRef(null);
+  let projects = useRef(null);
+  let blogs = useRef(null);
+
+  useEffect(() => {
+    contactIntro(contact);
+    contactInfoIntro(contactInfo);
+    emailIntro(email);
+
+    quote1Intro(qoute1);
+    quote2Intro(quote2);
+    detailsIntro(exp, projects, blogs);
+  });
   return (
     <div>
-      <h1>ContactMeSection Section...</h1>
+      <section className="profile_contact_section">
+        <div className="contact_and_qoute">
+          <div className="contact">
+            <p
+              className="contact_text"
+              ref={(ele) => {
+                contact = ele;
+              }}
+            >
+              - Contact
+            </p>
+            <p
+              className="contact_info"
+              ref={(ele) => {
+                contactInfo = ele;
+              }}
+            >
+              Any Type of Query & Discussion.
+            </p>
+            <p
+              ref={(ele) => {
+                email = ele;
+              }}
+            >
+              <Link
+                to="/email"
+                className="contact_email"
+                onMouseEnter={(e) => hoverLink(e)}
+                onMouseLeave={(e) => hoverExit(e)}
+                ref={(ele) => {
+                  link2 = ele;
+                }}
+              >
+                Saurabhbharti9@gmail.com
+              </Link>
+            </p>
+          </div>
+          <div className="quote">
+            <p
+              className="quote_1"
+              ref={(ele) => {
+                qoute1 = ele;
+              }}
+            >
+              In The End it's You Vs You.
+            </p>
+            <p
+              className="quote_2"
+              ref={(ele) => {
+                quote2 = ele;
+              }}
+            >
+              Don't be intimidated by success or failure, Instead get up and
+              start fullfilling your dreams again.
+            </p>
+            <div className="experience_projects_blogs">
+              <div>
+                <div
+                  className="large_font_div"
+                  ref={(ele) => {
+                    exp = ele;
+                  }}
+                >
+                  <span className="exp_number1">3</span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className="exp_word1">Years of Experience</span>
+                </div>
+              </div>
+              <div>
+                <div
+                  className="large_font_div"
+                  ref={(ele) => {
+                    projects = ele;
+                  }}
+                >
+                  <span className="exp_number">3</span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className="exp_word">Projects</span>
+                </div>
+              </div>
+              <div>
+                <div
+                  className="large_font_div"
+                  ref={(ele) => {
+                    blogs = ele;
+                  }}
+                >
+                  <span className="exp_number">3</span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className="exp_word">Blogs</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="scroll_down_arrow">
+          <img src={process.env.PUBLIC_URL + "/double-down-96.png"} alt="" />
+        </div>
+      </section>
     </div>
   );
 };
--- client/src/components/profile/IntroSection.js
+++ client/src/components/profile/IntroSection.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../../css/Profile.css";
 import NavBar from "./NavBar";
+import ContactMeSection from "./ContactMeSection";
 import {
   nameIntro,
   intro,
@@ -69,16 +70,6 @@ const IntroSection = () => {
     jobTitleIntro(jobTitle);
     jobDetailsIntro(jobDetails);
     storyIntro(story);
-
-    contactIntro(contact);
-    contactInfoIntro(contactInfo);
-    emailIntro(email);
-
-    quote1Intro(qoute1);
-    quote2Intro(quote2);
-    detailsIntro(exp, projects, blogs);
-
-    staggerLinks(link1, link2);
 
     sliderIntro(sliderText1, sliderText2, sliderText3);
   }, []);
@@ -154,105 +145,8 @@ const IntroSection = () => {
             </div>
           </div>
         </section>
-        <section className="profile_contact_section">
-          <div className="contact_and_qoute">
-            <div className="contact">
-              <p
-                className="contact_text"
-                ref={(ele) => {
-                  contact = ele;
-                }}
-              >
-                - Contact
-              </p>
-              <p
-                className="contact_info"
-                ref={(ele) => {
-                  contactInfo = ele;
-                }}
-              >
-                Any Type of Query & Discussion.
-              </p>
-              <p
-                ref={(ele) => {
-                  email = ele;
-                }}
-              >
-                <Link
-                  to="/email"
-                  className="contact_email"
-                  onMouseEnter={(e) => hoverLink(e)}
-                  onMouseLeave={(e) => hoverExit(e)}
-                  ref={(ele) => {
-                    link2 = ele;
-                  }}
-                >
-                  Saurabhbharti9@gmail.com
-                </Link>
-              </p>
-            </div>
-            <div className="quote">
-              <p
-                className="quote_1"
-                ref={(ele) => {
-                  qoute1 = ele;
-                }}
-              >
-                In The End it's You Vs You.
-              </p>
-              <p
-                className="quote_2"
-                ref={(ele) => {
-                  quote2 = ele;
-                }}
-              >
-                Don't be intimidated by success or failure, Instead get up and
-                start fullfilling your dreams again.
-              </p>
-              <div className="experience_projects_blogs">
-                <div>
-                  <div
-                    className="large_font_div"
-                    ref={(ele) => {
-                      exp = ele;
-                    }}
-                  >
-                    <span className="exp_number1">3</span>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <span className="exp_word1">Years of Experience</span>
-                  </div>
-                </div>
-                <div>
-                  <div
-                    className="large_font_div"
-                    ref={(ele) => {
-                      projects = ele;
-                    }}
-                  >
-                    <span className="exp_number">3</span>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <span className="exp_word">Projects</span>
-                  </div>
-                </div>
-                <div>
-                  <div
-                    className="large_font_div"
-                    ref={(ele) => {
-                      blogs = ele;
-                    }}
-                  >
-                    <span className="exp_number">3</span>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <span className="exp_word">Blogs</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="scroll_down_arrow">
-            <img src={process.env.PUBLIC_URL + "/double-down-96.png"} alt="" />
-          </div>
-        </section>
+        {/**contact section */}
+        <ContactMeSection />
         <section className="sec2">projects</section>
         <section className="sec2">Blogs</section>
         <section className="sec2">contacts</section>
