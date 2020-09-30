8a6d88a69f1b45c69d136130ab7da8d6e1b2b035
saurabh <saurabhbharti9@gmail.com>
2020-09-30 12:19:10 +0530

    added project card styles

--- client/src/components/profile/ProjectSection.js
+++ client/src/components/profile/ProjectSection.js
@@ -11,47 +11,49 @@ const ProjectSection = () => {
     jobProfileCardIntro(card_sec, card_stack, card_ml);
   });
   return (
-    <div className="project">
-      <div className="project__job__profile">
-        <div
-          className="project__job__profile__card color__sec"
-          ref={(ele) => {
-            card_sec = ele;
-          }}
-        >
-          <i
-            className="fa fa-shield job__profile__icons"
-            aria-hidden="true"
-          ></i>
-          <h3>Security Consultant</h3>
+    <>
+      <div className="project">
+        <div className="project__job__profile">
+          <div
+            className="project__job__profile__card color__sec"
+            ref={(ele) => {
+              card_sec = ele;
+            }}
+          >
+            <i
+              className="fa fa-shield job__profile__icons"
+              aria-hidden="true"
+            ></i>
+            <h3>Security Consultant</h3>
+          </div>
+          <div
+            className="project__job__profile__card"
+            ref={(ele) => {
+              card_stack = ele;
+            }}
+          >
+            <i
+              className="fa fa-desktop job__profile__icons"
+              aria-hidden="true"
+            ></i>
+            <h3>Full Stack Developer</h3>
+          </div>
+          <div
+            className="project__job__profile__card color__ml"
+            ref={(ele) => {
+              card_ml = ele;
+            }}
+          >
+            <i
+              className="fa fa-line-chart job__profile__icons"
+              aria-hidden="true"
+            ></i>
+            <h3>Machine Learning Enthuiast</h3>
+          </div>
         </div>
-        <div
-          className="project__job__profile__card"
-          ref={(ele) => {
-            card_stack = ele;
-          }}
-        >
-          <i
-            className="fa fa-desktop job__profile__icons"
-            aria-hidden="true"
-          ></i>
-          <h3>Full Stack Developer</h3>
-        </div>
-        <div
-          className="project__job__profile__card color__ml"
-          ref={(ele) => {
-            card_ml = ele;
-          }}
-        >
-          <i
-            className="fa fa-line-chart job__profile__icons"
-            aria-hidden="true"
-          ></i>
-          <h3>Machine Learning Enthuiast</h3>
-        </div>
+        {/**top 3 projects display */}
       </div>
-      {/**top 3 projects display */}
-      <div className="projects">
+      <div className="project__section">
         <div className="projects__intro_p1">
           <div className="project__intro">
             <h3>- AllProjects</h3>
@@ -59,27 +61,39 @@ const ProjectSection = () => {
             <Link to="allProjects">Explore More</Link>
           </div>
           <div className="project1__card">
-            <p>Twitter Clone</p>
-            <img
-              src={process.env.PUBLIC_URL + "/logo512.png"}
-              alt=""
-              className="project__image"
-            />
+            <p className="project__card__name">Twitter Clone</p>
+            <div>
+              <img
+                src={process.env.PUBLIC_URL + "/logo512.png"}
+                alt=""
+                className="project__image"
+              />
+            </div>
+            <code>- Built Tools</code>
             <div className="project__card__techstack">
               <p>
-                <img src={process.env.PUBLIC_URL + "/icons8-node-js-96.png"} />
+                <img
+                  src={process.env.PUBLIC_URL + "/icons8-node-js-96.png"}
+                  className="icon_img"
+                />
               </p>
               <p>
-                <img src={process.env.PUBLIC_URL + "/icons8-node-js-96.png"} />
+                <img
+                  src={process.env.PUBLIC_URL + "/icons8-react-100.png"}
+                  className="icon_img"
+                />
               </p>
               <p>
-                <img src={process.env.PUBLIC_URL + "/icons8-node-js-96.png"} />
+                <img
+                  src={process.env.PUBLIC_URL + "/icons8-mongodb-96.png"}
+                  className="icon_img"
+                />
               </p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default ProjectSection;
