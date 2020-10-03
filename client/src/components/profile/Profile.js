aaac4becb655ae8be919006adb601aaf775d179b
saurabh <saurabhbharti9@gmail.com>
2020-10-03 19:50:15 +0530

    tech-stack init

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,11 +2,13 @@ import React from "react";
 import IntroSection from "./IntroSection";
 import ProjectSection from "./ProjectSection";
 import ContactMeSection from "./ContactMeSection";
+import TechStackSection from "./TechStackSection";
 const Profile = () => {
   return (
     <div>
       <IntroSection />
       <ProjectSection />
+      <TechStackSection />
       <ContactMeSection />
     </div>
   );
--- client/src/components/profile/TechStackSection.js
+++ client/src/components/profile/TechStackSection.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "../../css/Tech.css";
 const TechStackSection = () => {
   return (
     <div>
       <h1>TechStackSection Section...</h1>
+      <codersrank-widget
+        username="iamsbharti"
+        class="my-widget"
+        badges="4"
+        style={{ width: "340px" }}
+      ></codersrank-widget>
     </div>
   );
 };
