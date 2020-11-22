559c37ab586426c2971f0d5ce20748d675f19626
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-22 19:41:06 +0530

    blog design refactyor

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -20,28 +20,55 @@ const Blogs = () => {
       <div className="blog__page">
         <p>Blogs...</p>
         <div className="blogs">
-          <div className="blog__content">
-            <div className="blog__content__topic">
-              <p>ML</p>
-            </div>
-            <div className="blog__content__title">
-              <p>Demistifying Pi Value</p>
-            </div>
-            <div className="blog__content__desc">
-              <p>
-                Lfhsdjfyuer djhfgsdjhgfgeyr dhfgsdyufyuwetu jhdfgsdjhfysd sdgfte
-                dfsdfgsjhd
-              </p>
-            </div>
-            <div className="blog__content__footer">
-              <div className="date">26 April 1995</div>
-              <div className={classes.root}>
-                <Chip color="primary" label="ML" size="small" />
+          <div className="blog__card">
+            <div className="blog__content">
+              <div className="blog__content__topic">
+                <p>ML</p>
+              </div>
+              <div className="blog__content__title">
+                <p>Demistifying Pi Value</p>
+              </div>
+              <div className="blog__content__desc">
+                <p>
+                  Lfhsdjfyuer djhfgsdjhgfgeyr dhfgsdyufyuwetu jhdfgsdjhfysd
+                  sdgfte dfsdfgsjhd
+                </p>
+              </div>
+              <div className="blog__content__footer">
+                <div className="date">26 April 1995</div>
+                <div className={classes.root}>
+                  <Chip color="primary" label="ML" size="small" />
+                </div>
               </div>
             </div>
+            <div className="blog__img">
+              <img src={process.env.PUBLIC_URL + "/blog1.jpg"} alt="blogs" />
+            </div>
           </div>
-          <div className="blog__img">
-            <img src={process.env.PUBLIC_URL + "/blog1.jpg"} alt="blogs" />
+          <div className="blog__card">
+            <div className="blog__content">
+              <div className="blog__content__topic">
+                <p>ML</p>
+              </div>
+              <div className="blog__content__title">
+                <p>Demistifying Pi Value</p>
+              </div>
+              <div className="blog__content__desc">
+                <p>
+                  Lfhsdjfyuer djhfgsdjhgfgeyr dhfgsdyufyuwetu jhdfgsdjhfysd
+                  sdgfte dfsdfgsjhd
+                </p>
+              </div>
+              <div className="blog__content__footer">
+                <div className="date">26 April 1995</div>
+                <div className={classes.root}>
+                  <Chip color="primary" label="ML" size="small" />
+                </div>
+              </div>
+            </div>
+            <div className="blog__img">
+              <img src={process.env.PUBLIC_URL + "/blog1.jpg"} alt="blogs" />
+            </div>
           </div>
         </div>
       </div>
