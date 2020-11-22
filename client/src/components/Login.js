38bde43c530d4e0b97b9affe09682364fb490ab3
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-22 20:09:55 +0530

    added login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../css/Login.css";
 
 const Login = () => {
-  return <div>Login page</div>;
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login__page">
+      <div className="login__content">
+        <form className="login__form">
+          <label for="loginId">LoginId</label>
+          <input
+            type="text"
+            name="loginId"
+            placeholder="userid"
+            value={loginId}
+            onClick={(event) => setLoginId(event.target.value)}
+          />
+          <label for="loginId">Secret</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Secret"
+            value={password}
+            onClick={(event) => setPassword(event.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Login;
