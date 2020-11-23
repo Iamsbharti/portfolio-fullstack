d4fc61d3395c8b73387d35b97c4ae34ebfd75c30
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-23 10:50:36 +0530

    added login api

--- client/src/api/apis.js
+++ client/src/api/apis.js
@@ -13,3 +13,40 @@ export const getAllProjects = async () => {
     console.warn("Error Fetching Projects");
   }
 };
+
+export const loginApi = async ({ loginId, password }) => {
+  console.log("Login api call");
+  //http://localhost:3001/api/v1/portfolio/login
+  try {
+    let loginRes = await axios.post(`${baseUrl}/api/v1/portfolio/login`, {
+      loginId: loginId,
+      password: password,
+    });
+
+    console.log("Login Response:", loginRes);
+    let {
+      firstName,
+      lastName,
+      userName,
+      userId,
+      authToken,
+    } = loginRes.data.data;
+    /**local storage */
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("authToken", authToken);
+    localStorage.setItem("name", firstName + " " + lastName);
+    localStorage.setItem("userName", userName);
+
+    let returnVal = loginRes.toObject();
+    delete returnVal.authToken;
+    returnVal = {
+      ...returnVal,
+      error: loginRes.error,
+      message: loginRes.message,
+    };
+    console.log("return value__login:", returnVal);
+    return returnVal;
+  } catch (error) {
+    console.warn("Error", error.message);
+  }
+};
