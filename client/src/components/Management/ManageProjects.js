8f4e7543400402ff81d7233dd673ea26559366d9
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-24 10:53:15 +0530

    started manage project design

--- client/src/components/Management/ManageProjects.js
+++ client/src/components/Management/ManageProjects.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import "../../css/Management.css";
+import { Button } from "@material-ui/core";
+import { AddBoxOutlined } from "@material-ui/icons";
 const ManageProjects = () => {
   let history = useHistory();
+  const handleAddProject = () => {};
   return (
     <div className="manage__page">
       <code>Manage Projects Console</code>
+      <div className="managae__content">
+        <Button onClick={handleAddProject}>
+          <AddBoxOutlined color="primary" />
+          <span style={{ color: "white" }}>Add Project</span>
+        </Button>
+      </div>
     </div>
   );
 };
