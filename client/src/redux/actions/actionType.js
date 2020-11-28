d907a17d5c08a6e2f9fa6167f1e00a369e1224de
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-28 15:05:31 +0530

    added create project action

--- client/src/redux/actions/actionType.js
+++ client/src/redux/actions/actionType.js
@@ -3,3 +3,4 @@ export const LOGIN_ERROR = "LOGIN_ERROR";
 export const LOGOUT = "LOGOUT";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_BLOGS = "GET_BLOGS";
+export const CREATE_PROJECT = "CREATE_PROJECT";
--- client/src/redux/actions/projectAction.js
+++ client/src/redux/actions/projectAction.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS } from "./actionType";
+import { CREATE_PROJECT, GET_PROJECTS } from "./actionType";
 
 import * as apis from "../../api/apis";
 
@@ -8,5 +8,13 @@ export function getAllProjectAction() {
     let allProjectsResponse = await apis.getAllProjects();
     console.log("all project response in action:", allProjectsResponse);
     dispatch({ type: GET_PROJECTS, allProjectsResponse });
+  };
+}
+export function createProjectAction(projectInfo) {
+  console.log("create project action", projectInfo);
+  return async (dispatch) => {
+    let newProject = await apis.createProject(projectInfo);
+    console.log("new project action res::", newProject);
+    dispatch({ type: CREATE_PROJECT, newProject });
   };
 }
