7c927fa3c558d52b8ff44bfcd4db95661c1521ce
Iamsbharti <saurabhbharti9@gmail.com>
2020-11-28 15:07:02 +0530

    updated reducer

--- client/src/redux/reducers/projectReducer.js
+++ client/src/redux/reducers/projectReducer.js
@@ -1,9 +1,11 @@
-import { GET_PROJECTS } from "../actions/actionType";
+import { CREATE_PROJECT, GET_PROJECTS } from "../actions/actionType";
 import { projects } from "../defaultStore";
 export function projectReducer(_projects = projects, action) {
   switch (action.type) {
     case GET_PROJECTS:
       return action.allProjectsResponse;
+    case CREATE_PROJECT:
+      return [..._projects, action.newProject];
     default:
       return _projects;
   }
