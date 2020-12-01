efd35137d984781be11b8a2fc73dc31bba2b215b
Iamsbharti <saurabhbharti9@gmail.com>
2020-12-01 12:08:50 +0530

    refactored create blog route

--- server/controller/blogControl.js
+++ server/controller/blogControl.js
@@ -6,7 +6,7 @@ const { deleteFile } = require("../initdb");
 
 const createBlog = async (req, res) => {
   logger.info("Create Blog Control");
-  const { userId, title, link, description } = req.body;
+  const { userId, title, link, description, type } = req.body;
   const newBlog = new Blog({
     blogId: shortid.generate(),
     userId: req.query.userId,
@@ -18,20 +18,31 @@ const createBlog = async (req, res) => {
 
   /**create blog */
   let blog;
-  Blog.create(newBlog, async (error, createdBlog) => {
-    console.log(error, createdBlog);
-    if (error) {
-      res
-        .status(500)
-        .json(formatResponse(true, 500, "Internal Server Error", error));
-    } else {
-      logger.info("populate image");
-      let blogs = await Blog.findOne({ blogId: createdBlog.blogId }).populate(
-        "image"
-      );
-      res.status(200).json(formatResponse(false, 200, "Blog Created", blogs));
-    }
-  });
+  let savedBlog = await Blog.create(newBlog);
+  const { blogId } = savedBlog;
+  // update type
+  let updateOptions = {
+    $push: {
+      type: { $each: type.split(",") },
+    },
+  };
+
+  console.log("updateoptions:", updateOptions);
+  let updatedBlog = await Blog.updateOne({ blogId: blogId }, updateOptions);
+  const { n } = updatedBlog;
+  let createdBlog = await Blog.findOne({
+    blogId: blogId,
+  }).populate("image");
+
+  if (createdBlog && n === 1) {
+    res
+      .status(200)
+      .json(formatResponse(false, 200, "Blog Created", createdBlog));
+  } else {
+    res
+      .status(500)
+      .json(formatResponse(true, 500, "Internal Server Error", null));
+  }
 };
 const getBlogs = async (req, res) => {
   logger.info("Get All Blog Control");
